8e23ef6538086ed39d3396c62d01070b03d915e5
itayz1e <itayz1e@gmail.com>
2024-07-01 22:20:47 +0300

    commit Initial commit

---
+++ Back-end-AI/src/main/java/com/Back_end_AI/Back_end_AI/BackEndAiApplication.java
@@ -0,0 +1,13 @@
+package com.Back_end_AI.Back_end_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndAiApplication.class, args);
+	}
+
+}
