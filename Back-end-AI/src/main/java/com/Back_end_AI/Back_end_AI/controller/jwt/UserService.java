3deac03c0cc9777bf705871d2138df9ac45f58b3
itayz1e <itayz1e@gmail.com>
2024-07-16 21:18:08 +0300

    Creating a user who receives a token and checking security with a token

---
+++ Back-end-AI/src/main/java/com/Back_end_AI/Back_end_AI/controller/jwt/UserService.java
@@ -0,0 +1,11 @@
+package com.Back_end_AI.Back_end_AI.controller.jwt;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+    public void save(DBUser user) {
+    }
+}
